2c8a6accb91c734558b3038ac11c487d4c66984d
aleenya <anya.aleenaa@gmail.com>
2026-08-11 13:23:40 +0700

    feat: menus logic

---
+++ backend/menus/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import DefaultRouter
+from .views import MenuViewSet
+ 
+router = DefaultRouter()
+router.register("menus", MenuViewSet, basename="menu")
+urlpatterns = router.urls
