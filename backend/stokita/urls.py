621393de267499bbee0b05fb71c719003f6e37a1
aleenya <anya.aleenaa@gmail.com>
2026-08-11 13:13:18 +0700

    feat: inventory logic

--- backend/stokita/urls.py
+++ backend/stokita/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/v1/", include("inventory.urls")),
+    path("api/v1/", include("menus.urls")),
+    path("api/v1/", include("sales.urls")),
+    path("api/v1/", include("briefs.urls")),
 ]
