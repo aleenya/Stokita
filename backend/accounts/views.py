49c1543cbcb1f418e2fbf23391099f473213dffc
unknown <fini.ardiyanti@gmail.com>
2026-08-12 14:49:07 +0700

    trigger changes

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,12 +1,40 @@
-from rest_framework import viewsets, status
+from rest_framework import status, viewsets
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import User, StaffFeatureGrant
-from .serializers import StaffSerializer
-from .permissions import IsOwner
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+
+from .models import StaffFeatureGrant, User
+from .permissions import IsOwner
+from .serializers import BusinessSerializer, RegisterSerializer, StaffSerializer, UserSerializer
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response(
+            {
+                "token": token.key,
+                "role": user.role,
+                "business": BusinessSerializer(user.business).data,
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
 
 
 class StaffViewSet(viewsets.ViewSet):
@@ -36,9 +64,3 @@ class StaffViewSet(viewsets.ViewSet):
             StaffFeatureGrant.objects.create(staff=staff, feature=f, granted_by=request.user)
 
         return Response(StaffSerializer(staff).data)
-
-class MeView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response(UserSerializer(request.user).data)
