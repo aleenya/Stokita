18e86798d163a2054b5782f0978f4bdc614aa55f
aleenya <anya.aleenaa@gmail.com>
2026-08-13 15:28:08 +0700

    fix deploy problem

--- backend/stokita/urls.py
+++ backend/stokita/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
 from accounts.views import (
     CsrfCookieView, GoogleLinkView, GoogleLoginView, LoginView, LogoutView, RefreshView, RegisterView,
 )
@@ -35,7 +34,4 @@ urlpatterns = [
     path("api/v1/auth/register/", RegisterView.as_view()),
     path("api/v1/auth/google/login/", GoogleLoginView.as_view()),
     path("api/v1/auth/google/link/", GoogleLinkView.as_view()),
-    
-    # Catch-all route for React SPA fallback
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
